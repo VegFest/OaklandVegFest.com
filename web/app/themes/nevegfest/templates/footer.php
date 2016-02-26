e29391c16732a2db2437aa361103a4e764a0daa2
drewrwilson <drew.r.wilson@gmail.com>
2016-02-26 17:35:48 -0500

    change to oakland mailchimp

--- web/app/themes/nevegfest/templates/footer.php
+++ web/app/themes/nevegfest/templates/footer.php
@@ -18,7 +18,7 @@
 					<h3>Signup for VegFest News</h3>
 					<!-- Begin MailChimp Signup Form -->
 					<div id="mc_embed_signup">
-					<form action="//VegFest.us2.list-manage.com/subscribe/post?u=38963ea1aa7f978ae2c06864a&amp;id=b1be298b15" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<form action="//oaklandveg.us4.list-manage.com/subscribe/post?u=fc673914e26006f00e11d0c44&amp;id=3038a44a7e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
 
 <div class="mc-field-group">
